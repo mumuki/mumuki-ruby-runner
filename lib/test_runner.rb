da7c431e3076733c5396a5bd1fc6f595ae786855
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-08-16 19:53:22 -0300

    Extracting Sandbox

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -1,40 +1,14 @@
 require 'docker'
 require 'pathname'
 
-class TestRunner < Mumukit::FileTestRunner
-  def rspec_path
-    config['rspec_command']
-  end
+class TestSandbox
+  attr_accessor :container
 
-  def run_test_file!(file)
-    container = create_container(file)
-
-    run_container(container)
-
-    exit = container.json['State']['ExitCode']
-    logs = container.streaming_logs(stdout: true, stderr: true)
-
-    if exit == 0
-      [logs, :passed]
-    else
-      [logs, :failed]
-    end
-  rescue Docker::Error::TimeoutError => e
-    [I18n.t('mumukit.time_exceeded', limit: Mumukit.config.command_time_limit), :aborted]
-  ensure
-    destroy_container(container)
-  end
-
-  def destroy_container(container)
-    container.stop
-    container.delete
-  end
-
-  def create_container(file)
+  def configure!(file)
     filename = File.absolute_path file.path
     pathname = Pathname.new(filename)
 
-    Docker::Container.create(
+    self.container = Docker::Container.create(
         'Image' => 'abdd878dd50a',
         'Cmd' => ['rspec', "#{filename}", '-f', 'json'],
         'HostConfig' => {
@@ -43,12 +17,49 @@ class TestRunner < Mumukit::FileTestRunner
             pathname.dirname => {}})
   end
 
-  def run_container(container)
+  def run!
     container.start
     container.wait(Mumukit.config.command_time_limit)
   end
 
+  def destroy!
+    container.stop
+    container.delete
+  end
 
+  def exit_code
+    container.json['State']['ExitCode']
+  end
+
+  def out
+    container.streaming_logs(stdout: true, stderr: true)
+  end
+
+end
+
+class TestRunner < Mumukit::FileTestRunner
+  def rspec_path
+    config['rspec_command']
+  end
+
+  def run_test_file!(file)
+    sandbox = TestSandbox.new
+    sandbox.configure! file
+    sandbox.run!
+
+    exit = sandbox.exit_code
+    out = sandbox.out
+
+    if exit == 0
+      [out, :passed]
+    else
+      [out, :failed]
+    end
+  rescue Docker::Error::TimeoutError => e
+    [I18n.t('mumukit.time_exceeded', limit: Mumukit.config.command_time_limit), :aborted]
+  ensure
+    sandbox.destroy!
+  end
 
   #------------------
 
