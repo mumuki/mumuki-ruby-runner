d2224c2de4ae7f5a5831da9c1457fa100e7af74e
Agustin Pina <aguspina87@gmail.com>
2016-11-22 14:35:00 -0300

    Add spec

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -23,4 +23,13 @@ describe RubyQueryHook do
     let(:request) { struct(query: 'y', extra: 'y=64+2') }
     it { expect(result[0]).to eq "=> 66\n" }
   end
+
+  context 'query and cookie' do
+    let(:request) { struct(query: 'y + 1', cookie: ['y=64']) }
+    it { expect(result[0]).to eq "=> 65\n" }
+  end
+  context 'query and failed cookie' do
+    let(:request) { struct(query: 'y', cookie: ['y=5', 'raise a']) }
+    it { expect(result[0]).to eq "=> 5\n" }
+  end
 end
