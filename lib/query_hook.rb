3295981ad027d204bf4e01a7425d1c3b43ce8e88
Agustin Pina <aguspina87@gmail.com>
2016-11-22 14:34:55 -0300

    Support cookies

--- lib/query_hook.rb
+++ lib/query_hook.rb
@@ -6,10 +6,39 @@ class RubyQueryHook < Mumukit::Templates::FileHook
   end
 
   def compile_file_content(r)
-    "#{r.extra}\n#{r.content}\nprint('=> ' + (#{r.query}).inspect)"
+    <<ruby
+    #{r.extra}
+    #{r.content}
+    #{compile_cookie(r.cookie)}
+    #{compile_query(r.query)}
+ruby
   end
 
   def command_line(filename)
     "ruby #{filename}"
   end
+
+  def compile_query(query)
+    if query.start_with? 'def '
+      "#{query}\nputs \"<function>\""
+    else
+      "print('=> ' + (#{query}).inspect)"
+    end
+  end
+
+  def build_state(cookie)
+    (cookie||[]).map do |statement|
+    <<ruby
+  begin
+    #{statement}
+  rescue
+  end
+ruby
+    end
+  end
+
+  def compile_cookie(cookie)
+    build_state(cookie).join("\n")
+  end
+
 end
