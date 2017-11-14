b35eef5f78538e024a4c03f90e15e3fba88d5132
Julian Berbel Alt <julianberbel@hotmail.com>
2017-11-14 10:25:03 -0300

    Add rspec template to metadata

--- lib/metadata_hook.rb
+++ lib/metadata_hook.rb
@@ -10,7 +10,14 @@ class RubyMetadataHook < Mumukit::Hook
      test_framework: {
          name: 'rspec',
          version: '2.13',
-         test_extension: 'rb'
+         test_extension: 'rb',
+         template: <<ruby
+describe '{{ test_template_group_description }}' do
+  it '{{ test_template_sample_description }}' do
+    expect(true).to eq true
+  end
+end
+ruby
      }}
   end
 end
